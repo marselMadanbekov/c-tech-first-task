1f0594c7f86e39d66827b7d75aebb31f91f7136b
Madanbekov Marsel <madanbekovmarsel@gmail.com>
2026-01-22 19:15:03 +0600

    initial commit

---
+++ src/main/java/ctech/firsttask/ctechfirsttask/CTechFirstTaskApplication.java
@@ -0,0 +1,13 @@
+package ctech.firsttask.ctechfirsttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CTechFirstTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CTechFirstTaskApplication.class, args);
+    }
+
+}
